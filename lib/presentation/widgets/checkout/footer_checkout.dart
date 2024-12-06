d09fd016bf758ff988668f288904bba596664743
GelArria <arriamiguel9@gmail.com>
2024-12-06 15:29:55 -0400

    :pencil: added entire Checkout use case logic but not applied still.

--- lib/presentation/widgets/checkout/footer_checkout.dart
+++ lib/presentation/widgets/checkout/footer_checkout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ContinueButton extends StatelessWidget {
   const ContinueButton({super.key});
@@ -9,7 +10,7 @@ class ContinueButton extends StatelessWidget {
       width: double.infinity, // Para hacer que el botón ocupe todo el ancho
       child: ElevatedButton(
         onPressed: () {
-          // Acción al presionar el botón
+            context.go('/order');
         },
         style: ElevatedButton.styleFrom(
           backgroundColor: const Color(0xFF2000B1), // Azul
