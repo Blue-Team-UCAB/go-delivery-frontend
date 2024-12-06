a6eee7ae00f02c4d78092f2d33838cb2d0d31c8b
GustavoFF7 <gafreitas.19@est.ucab.edu.ve>
2024-12-05 22:25:18 -0400

    :art: Refactor empty state assets and add CartEmptyStateWidget

--- lib/presentation/widgets/cart/cart_empty_state_widget.dart
+++ lib/presentation/widgets/cart/cart_empty_state_widget.dart
@@ -1,10 +1,8 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class CouponEmptyStateWidget extends StatelessWidget {
-  const CouponEmptyStateWidget({super.key});
+class CartEmptyStateWidget extends StatelessWidget {
+  const CartEmptyStateWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +11,7 @@ class CouponEmptyStateWidget extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           SvgPicture.asset(
-            'assets/empty_states/coupon_emptystate.svg',
+            'assets/empty_states/cart_emptystate.svg',
             width: MediaQuery.of(context).size.width*0.75,
             height: MediaQuery.of(context).size.width*0.75,
             fit: BoxFit.fill,
@@ -22,9 +20,17 @@ class CouponEmptyStateWidget extends StatelessWidget {
           SizedBox(
             width: MediaQuery.of(context).size.width*0.75,
             child: const Text(
-              'Aun no has agregado ningun cupon',
+              'Tu Carrito esta vacio',
               textAlign: TextAlign.center,
               style: TextStyle(fontFamily: 'Montserrat',fontSize: 24,fontWeight: FontWeight.w700 ,color: Color(0xFF000000))
+            ),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width*0.75,
+            child: const Text(
+              'Parece que aún no has agregado nada en tu carrito',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontFamily: 'Montserrat',fontSize: 16,fontWeight: FontWeight.w400 ,color: Color(0xFFC3C3C3))
             ),
           ),
           SizedBox(height: MediaQuery.of(context).size.height*0.10,)
