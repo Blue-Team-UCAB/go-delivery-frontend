5fea3e24e1beb8d95a814ed080a8ed19addad55d
GustavoFF7 <gafreitas.19@est.ucab.edu.ve>
2025-01-05 21:57:29 -0400

    :art: Enhance homescreen placeholders with shimmer effect and update dependencies

--- lib/presentation/widgets/placeholders/bundle_card_placeholder.dart
+++ lib/presentation/widgets/placeholders/bundle_card_placeholder.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_delivery_frontend/presentation/widgets/placeholders/text_placeholder.dart';
+import 'package:shimmer/shimmer.dart';
 
 class BundleCardPlaceholder extends StatelessWidget {
   const BundleCardPlaceholder({
@@ -16,43 +17,47 @@ class BundleCardPlaceholder extends StatelessWidget {
           color: Color(0xFFFFFFFF),
           borderRadius: BorderRadius.all(Radius.circular(14.0)),
         ),
-        child: Column(
-          children: [
-            ClipRRect(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(12.0),
-                topRight: Radius.circular(12.0),
-              ),
-              child: Container(
-                  height: 94,
-                  color: const Color(0xFFd8d5dd),
+        child: Shimmer.fromColors(
+          baseColor: const Color(0xFFd8d5dd),
+          highlightColor: const Color(0xFFF4F4F4),
+          child: Column(
+            children: [
+              ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(12.0),
+                  topRight: Radius.circular(12.0),
                 ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const TextPlaceholder(height: 14,width: 120,),
-                  const SizedBox(height: 8,),
-                  const TextPlaceholder(height: 18,width: 40,),
-                  const SizedBox(height: 18,),
-                  const TextPlaceholder(height: 12),
-                  const SizedBox(height: 6,),
-                  const TextPlaceholder(height: 12,width: 80,),
-                  const SizedBox(height: 22,),
-                  Container(
-                    height: 40,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.rectangle,
-                      borderRadius: const BorderRadius.all(Radius.circular(12)),
-                      border: Border.all(color: const Color(0xFFd8d5dd),width: 4)
-                    ),
-                  )
-                ],
+                child: Container(
+                    height: 94,
+                    color: const Color(0xFFd8d5dd),
+                  ),
               ),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.all(15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const TextPlaceholder(height: 14,width: 120,),
+                    const SizedBox(height: 8,),
+                    const TextPlaceholder(height: 18,width: 40,),
+                    const SizedBox(height: 18,),
+                    const TextPlaceholder(height: 12),
+                    const SizedBox(height: 6,),
+                    const TextPlaceholder(height: 12,width: 80,),
+                    const SizedBox(height: 22,),
+                    Container(
+                      height: 40,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        borderRadius: const BorderRadius.all(Radius.circular(12)),
+                        border: Border.all(color: const Color(0xFFd8d5dd),width: 4)
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/presentation/widgets/placeholders/popular_product_card_placeholder.dart
+++ lib/presentation/widgets/placeholders/popular_product_card_placeholder.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_delivery_frontend/presentation/widgets/placeholders/text_placeholder.dart';
+import 'package:shimmer/shimmer.dart';
 
 class PopularProductCardPlaceholder extends StatelessWidget {
   const PopularProductCardPlaceholder({
@@ -15,38 +16,42 @@ class PopularProductCardPlaceholder extends StatelessWidget {
           borderRadius: BorderRadius.all(Radius.circular(12)),
           color: Color(0xFFFFFFFF)
         ),
-        child: Row(
-          children: [
-            Container(
-              height: 78,
-              width: 78,
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(topLeft:Radius.circular(12),bottomLeft:Radius.circular(12)),
-                color: Color(0xFFd8d5dd)
+        child: Shimmer.fromColors(
+          baseColor: Color(0xFFd8d5dd),
+          highlightColor: Color(0xFFF4F4F4),
+          child: Row(
+            children: [
+              Container(
+                height: 78,
+                width: 78,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(topLeft:Radius.circular(12),bottomLeft:Radius.circular(12)),
+                  color: Color(0xFFd8d5dd)
+                ),
               ),
-            ),
-            const SizedBox(width: 16,),
-            const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextPlaceholder(height: 14,width: 150,),
-                SizedBox(height: 6,),
-                TextPlaceholder(height: 14,width: 110,),
-                SizedBox(height: 8,),
-                TextPlaceholder(height: 12, width: 40,),
-              ],
-            ),
-            const SizedBox(width: 26,),
-            Container(
-              height: 40,
-              width: 80,
-              decoration: BoxDecoration(
-                shape: BoxShape.rectangle,
-                borderRadius: const BorderRadius.all(Radius.circular(12)),
-                border: Border.all(color: const Color(0xFFd8d5dd),width: 4)
+              const SizedBox(width: 16,),
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextPlaceholder(height: 14,width: 150,),
+                  SizedBox(height: 6,),
+                  TextPlaceholder(height: 14,width: 110,),
+                  SizedBox(height: 8,),
+                  TextPlaceholder(height: 12, width: 40,),
+                ],
               ),
-            ),
-          ],
+              const SizedBox(width: 26,),
+              Container(
+                height: 40,
+                width: 80,
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  borderRadius: const BorderRadius.all(Radius.circular(12)),
+                  border: Border.all(color: const Color(0xFFd8d5dd),width: 4)
+                ),
+              ),
+            ],
+          ),
         ),
       )
     );
