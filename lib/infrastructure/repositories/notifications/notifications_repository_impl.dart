167cf3e5b175b68f954cda743aee355bbacd1269
maberrios <maberrios.19@est.ucab.edu.ve>
2025-01-10 11:48:37 -0400

    minor fix for notifications endpoint

--- lib/infrastructure/repositories/notifications/notifications_repository_impl.dart
+++ lib/infrastructure/repositories/notifications/notifications_repository_impl.dart
@@ -21,7 +21,7 @@ class NotificationsRepositoryImpl implements NotificationsRepository {
     var message;
 
     final response = await _apiRequestManager.request<bool>(
-      '/api/Notifications/push-token',
+      '/api/Notifications/savetoken',
       'POST',
       (data) {
         if (data['errorCode'] != 200) {
@@ -79,4 +79,3 @@ class NotificationsRepositoryImpl implements NotificationsRepository {
     throw UnimplementedError();
   }
 }
-
