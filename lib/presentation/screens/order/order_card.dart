30d29424ad74525809ad6a1a36251333c57f8022
GustavoFF7 <gafreitas.19@est.ucab.edu.ve>
2025-01-12 00:44:10 -0400

    .

--- lib/presentation/screens/order/order_card.dart
+++ lib/presentation/screens/order/order_card.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:go_delivery_frontend/application/BLoc/order/order_cancel/order_cancel_bloc.dart';
 import 'package:go_delivery_frontend/infrastructure/models/order_many_model.dart';
+import 'package:go_delivery_frontend/presentation/widgets/order_detailed/past/report_problem_window.dart';
 import 'package:go_router/go_router.dart';
 import 'package:go_delivery_frontend/application/BLoc/order/order_cancel/order_cancel_event.dart';
 import 'package:go_delivery_frontend/application/BLoc/order/order_cancel/order_cancel_state.dart';
